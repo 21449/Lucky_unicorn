cb41f191f31ac3f06da6eefbec2191f031078b82
21449 <129801906+21449@users.noreply.github.com>
2023-05-02 11:13:41 +1200

    Update 01_yes_no_v1.py

--- 01_yes_no_v1.py
+++ 01_yes_no_v1.py
@@ -1,11 +1,13 @@
-show_instructions = input("have you played this game " "before? ").lower()
-if show_instructions == "yes": 
-  print("program continues")
-elif show_instructions == "y": 
-  print("program continues ")
-elif show_instructions == "no": 
-  print ("display instructions")
-elif show_instructions == "n": 
-  print ("display instructions")
-else: 
-  print ("Please answer yes / no")
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+  show_instructions = input("have you played this game " "before? ").lower()
+  if show_instructions == "yes": 
+    print("program continues")
+  elif show_instructions == "y": 
+    print("program continues ")
+  elif show_instructions == "no": 
+    print ("display instructions")
+  elif show_instructions == "n": 
+    print ("display instructions")
+  else: 
+    print ("Please answer yes / no")
